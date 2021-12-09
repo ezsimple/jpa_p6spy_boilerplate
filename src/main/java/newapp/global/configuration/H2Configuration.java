a3c9fd2246aee55504ffd1c2c87d5e9f00627d42
mhlee <mkeasy@gmail.com>
2021-12-09 15:46:33 +0900

    .

---
+++ src/main/java/newapp/global/configuration/H2Configuration.java
@@ -0,0 +1,30 @@
+package newapp.global.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@Profile("local")
+public class H2Configuration {
+
+  final int port = 9092;
+
+  @Bean
+  @ConfigurationProperties("spring.datasource.hikari")
+  public DataSource dataSource() throws SQLException {
+    Server.createTcpServer(
+      "-tcp",
+      "-tcpAllowOthers",
+      "-ifNotExists",
+      "-tcpPort", String.valueOf(port)).start();
+    return new HikariDataSource();
+  }
+
+}

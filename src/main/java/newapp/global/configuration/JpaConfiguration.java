6629c31b85e3879bb78239939926c1d180cf9202
mhlee <mkeasy@gmail.com>
2021-12-09 14:35:05 +0900

    .

--- src/main/java/newapp/global/configuration/JpaConfiguration.java
+++ src/main/java/newapp/global/configuration/JpaConfiguration.java
@@ -1,21 +1,26 @@
 package newapp.global.configuration;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 @Configuration
 public class JpaConfiguration {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+  @PersistenceContext
+  private EntityManager entityManager;
 
-    @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
-    }
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
 
 }

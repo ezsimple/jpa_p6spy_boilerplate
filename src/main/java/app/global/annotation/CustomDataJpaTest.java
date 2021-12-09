80cf665ce39aa2af96f5d4c2a009d840c2b06097
mhlee <mkeasy@gmail.com>
2021-12-09 12:07:22 +0900

    .

---
+++ src/main/java/app/global/annotation/CustomDataJpaTest.java
@@ -0,0 +1,19 @@
+package app.global.annotation;
+
+import com.github.gavlyukovskiy.boot.jdbc.decorator.DataSourceDecoratorAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@DataJpaTest(showSql = false)
+@ImportAutoConfiguration(DataSourceDecoratorAutoConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// @Import(P6spyConfiguration.class)
+@interface CustomDataJpaTest {
+}

27d8ed3a2735a09f121a1d260e12b8f9c19e58c5
mhlee <mkeasy@gmail.com>
2021-12-10 11:55:28 +0900

    .

---
+++ src/main/java/newapp/global/support/SessionSupport.java
@@ -0,0 +1,33 @@
+package newapp.global.support;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+public class SessionSupport {
+
+    public static void setAttribute(String name, String value) {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        request.getSession().setAttribute(name, value);
+    }
+
+    public static String getAttribute(String name, String defaultValue) {
+
+        return SessionSupport.getAttribute(name).orElse(defaultValue);
+    }
+
+    public static Optional<String> getAttribute(String name) {
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        Object value = request.getSession().getAttribute(name);
+        if (value == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(value.toString());
+        }
+    }
+}

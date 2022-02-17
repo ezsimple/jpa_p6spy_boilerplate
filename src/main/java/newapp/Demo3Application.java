4d800f4b0e6ecdec42faed1fce4dc4b98f36fb33
mhlee <mhlee@ezfarm.co.kr>
2022-02-17 23:08:55 +0900

    .

--- src/main/java/newapp/Demo3Application.java
+++ src/main/java/newapp/Demo3Application.java
@@ -3,11 +3,14 @@ package newapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @RequiredArgsConstructor
 @SpringBootApplication
 public class Demo3Application {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Demo3Application.class, args);
-  }
+    // private final QueryFactory queryFactory;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo3Application.class, args);
+    }
 
 }

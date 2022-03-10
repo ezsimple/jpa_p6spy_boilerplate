7e57f74aaf4804d5d10e2ee76a572ab3b542023f
mhlee <mkeasy@gmail.com>
2022-03-10 13:10:27 +0900

    .

--- src/main/java/newapp/domain/biz/controller/LoginController.java
+++ src/main/java/newapp/domain/biz/controller/LoginController.java
@@ -5,53 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Controller
 public class LoginController {
 
     /**
-     * Google OAuth2 redirect address
-     * @param model
+     * provider OAuth2 redirect address
+     * providers : google, facebook, naver, kakao
      * @param commandMap
      * @return
      */
-    @GetMapping("/login/oauth2/code/google")
-    public String google(Model model, CommandMap commandMap) {
-        return "login/google";
+    @GetMapping("/login/oauth2/code/{provider}")
+    public String redirect(@PathVariable("provider") String provider, CommandMap commandMap) {
+        return "login/"+provider;
     }
 
-    /**
-     * Facebook OAuth2 redirect address
-     * @param model
-     * @param commandMap
-     * @return
-     */
-    @GetMapping("/login/oauth2/code/facebook")
-    public String facebook(Model model, CommandMap commandMap) {
-        return "login/facebook";
-    }
-
-    /**
-     * Naver OAuth2 redirect address
-     * @param model
-     * @param commandMap
-     * @return
-     */
-    @GetMapping("/login/oauth2/code/naver")
-    public String naver(Model model, CommandMap commandMap) {
-        return "login/naver";
-    }
-
-    /**
-     * Kakao OAuth2 redirect address
-     * email 정보가 민감정보라니 .... ㄷㄷ
-     * @param model
-     * @param commandMap
-     * @return
-     */
-    @GetMapping("/login/oauth2/code/kakao")
-    public String kakao(Model model, CommandMap commandMap) {
-        return "login/kakao";
-    }
 }

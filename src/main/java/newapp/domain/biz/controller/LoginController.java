fdb0fdb5ada4280ca6939553f18a1617c0e4d93d
mhlee <mhlee@ezfarm.co.kr>
2022-03-12 14:48:04 +0900

    .

--- src/main/java/newapp/domain/biz/controller/LoginController.java
+++ src/main/java/newapp/domain/biz/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -20,6 +21,21 @@ public class LoginController {
     @GetMapping("/login/oauth2/code/{provider}")
     public String redirect(@PathVariable("provider") String provider, CommandMap commandMap) {
         return "login/"+provider;
+    }
+
+    @GetMapping("/sign_in.do")
+    public String signIn(Model model, CommandMap commandMap) {
+        return "login/sign_in";
+    }
+
+    @PostMapping("/sign_ok.do")
+    public String signOk(Model model, CommandMap commandMap) {
+        return "login/sign_in";
+    }
+
+    @GetMapping("/sign_out.do")
+    public String signOut(Model model, CommandMap commandMap) {
+        return "login/sign_out";
     }
 
 }

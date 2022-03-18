cd805a51500ef93a20891e0b6e99bb6643831bc2
zmon-insoft <94100763+zmon-insoft@users.noreply.github.com>
2022-03-18 16:20:11 +0900

    .

--- src/main/java/newapp/global/configuration/H2Configuration.java
+++ src/main/java/newapp/global/configuration/H2Configuration.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Slf4j
-@Configuration
-@Profile("local")
-@RequiredArgsConstructor
+//@Slf4j
+//@Configuration
+//@Profile("local")
+//@RequiredArgsConstructor
 public class H2Configuration {
 
 //  @Bean
